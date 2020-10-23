f7a26997e32f87239e18daf517562037c16076c7
Tusharkanti Ghosh <44294394+tghosh30@users.noreply.github.com>
2020-10-23 02:37:22 -0600

    Added tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(Marr)
+
+test_check("Marr")
---
+++ tests/testthat/test-testthat.R
@@ -0,0 +1,31 @@
+library(testthat)
+context("checking the Marr function")
+
+library(Marr)
+
+test_that("checking dimensions of Marr object", {
+        n_features <- 1200
+        n_samples <- 20
+        set.seed(3456)
+        data_test <- matrix(rnorm(n_features * n_samples),
+        n_features, n_samples)
+        Marr_test <- Marr(object = data_test)
+
+        expect_equal(length(MarrSamplepairs(Marr_test)),
+        choose(n_samples,2))
+        expect_equal(length(MarrFeatures(Marr_test)), n_features)
+})
+
+test_that("checking Marr data", {
+        n_features <- 1200
+        n_samples <- 20
+        set.seed(3456)
+        data_test <- matrix(rnorm(n_features * n_samples),
+        n_features, n_samples)
+        Marr_test <- Marr(object = data_test)
+
+        expect_equal(MarrSamplepairs(Marr_test)[184], 0.08333333,
+        tolerance = 1e-06)
+        expect_equal(MarrFeatures(Marr_test)[879], 0.5263158,
+        tolerance = 1e-06)
+})
