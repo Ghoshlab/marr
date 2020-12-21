f297f0da077b9eadd01136b31cab66c75be5559c
Tusharkanti Ghosh <44294394+tghosh30@users.noreply.github.com>
2020-12-21 16:41:47 -0700

    generic alpha

--- R/MarrProc.R
+++ R/MarrProc.R
@@ -50,7 +50,7 @@ MarrProc <- function(object, alpha = 0.05) {
                 for (j in i:dim(object)[2]) {
                         if (i < j) {
                                 listMarr <- .MaRR(datMaxRankij[i, j, ],
-                                alpha = 0.05)
+                                alpha)
                                 rall[i, j] <- length(listMarr[[5]])
                                 mall_rep_i <- listMarr[[5]]
                                 posall <- array(0, dim = c(dim(object)[1]))
