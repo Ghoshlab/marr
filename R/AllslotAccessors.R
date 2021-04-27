2e498016fac999b92d77bc4b355058fc90ea1eaf
Max McGrath <max.a.mcgrath@gmail.com>
2021-04-27 15:24:00 -0600

    Adds slot accessors for new slots

--- R/AllslotAccessors.R
+++ R/AllslotAccessors.R
@@ -28,6 +28,7 @@ setMethod(f = "MarrSamplepairs", signature(object = "Marr"),
             function(object) {
                         return(object@MarrSamplepairs)
             })
+
 #' @title Accessors for the 'MarrFeatures' slot of a Marr object.
 #'
 #' @description Accessors for the 'MarrFeatures' slot
@@ -58,6 +59,7 @@ setMethod(f = "MarrFeatures", signature(object = "Marr"),
             function(object) {
                         return(object@MarrFeatures)
             })
+
 #' @title Accessors for the 'MarrSamplepairsfiltered' slot of a Marr object.
 #'
 #' @description Accessors for the 'MarrSamplepairsfiltered'
@@ -87,6 +89,7 @@ setMethod(f = "MarrSamplepairsfiltered", signature(object = "Marr"),
             function(object) {
                         return(object@MarrSamplepairsfiltered)
             })
+
 #' @title Accessors for the 'MarrFeaturesfiltered' slot of a Marr object.
 #'
 #' @description Accessors for the 'MarrFeaturesfiltered'
@@ -116,3 +119,151 @@ setMethod(f = "MarrFeaturesfiltered", signature(object = "Marr"),
             function(object) {
                         return(object@MarrFeaturesfiltered)
             })
+
+#' @title Accessors for the 'MarrData' slot of a Marr object.
+#'
+#' @description Accessors for the 'MarrData'
+#' slot of a Marr object.
+#'
+#' @usage
+#' \S4method{MarrData}{Marr}(object)
+#'
+#' @docType methods
+#' @name MarrData
+#' @rdname MarrData
+#' @aliases MarrData MarrData,Marr-method
+#' @param object an object of class \code{Marr}.
+#'
+#' @return  Original data object passed to \code{Marr}
+#'
+#' @export
+#'
+#' @examples
+#' data <- matrix(rnorm(2400), nrow=200, ncol=12)
+#' data_Marr <- Marr(object = data, pSamplepairs=0.75,
+#'                   pFeatures=0.75, alpha=0.05)
+#' MarrData(data_Marr)
+#'
+setMethod(f = "MarrData", signature(object = "Marr"),
+          function(object) {
+              return(object@MarrData)
+          })
+
+#' @title Accessors for the 'MarrPSamplepairs' slot of a Marr object.
+#'
+#' @description Accessors for the 'MarrPSamplepairs'
+#' slot of a Marr object.
+#'
+#' @usage
+#' \S4method{MarrPSamplepairs}{Marr}(object)
+#'
+#' @docType methods
+#' @name MarrPSamplepairs
+#' @rdname MarrPSamplepairs
+#' @aliases MarrPSamplepairs MarrPSamplepairs,Marr-method
+#' @param object an object of class \code{Marr}.
+#'
+#' @return  Value of \code{pSamplepairs} argument passed 
+#' to \code{Marr}
+#'
+#' @export
+#'
+#' @examples
+#' data <- matrix(rnorm(2400), nrow=200, ncol=12)
+#' data_Marr <- Marr(object = data, pSamplepairs=0.75,
+#'                   pFeatures=0.75, alpha=0.05)
+#' MarrPSamplepairs(data_Marr)
+#'
+setMethod(f = "MarrPSamplepairs", signature(object = "Marr"),
+          function(object) {
+              return(object@MarrPSamplepairs)
+          })
+
+#' @title Accessors for the 'MarrPFeatures' slot of a Marr object.
+#'
+#' @description Accessors for the 'MarrPFeatures'
+#' slot of a Marr object.
+#'
+#' @usage
+#' \S4method{MarrPFeatures}{Marr}(object)
+#'
+#' @docType methods
+#' @name MarrPFeatures
+#' @rdname MarrPFeatures
+#' @aliases MarrPFeatures MarrPFeatures,Marr-method
+#' @param object an object of class \code{Marr}.
+#'
+#' @return  Value of \code{MarrPFeatures} argument passed 
+#' to \code{Marr}
+#'
+#' @export
+#'
+#' @examples
+#' data <- matrix(rnorm(2400), nrow=200, ncol=12)
+#' data_Marr <- Marr(object = data, pSamplepairs=0.75,
+#'                   pFeatures=0.75, alpha=0.05)
+#' MarrPFeatures(data_Marr)
+#'
+setMethod(f = "MarrPFeatures", signature(object = "Marr"),
+          function(object) {
+              return(object@MarrPFeatures)
+          })
+
+#' @title Accessors for the 'MarrAlpha' slot of a Marr object.
+#'
+#' @description Accessors for the 'MarrAlpha'
+#' slot of a Marr object.
+#'
+#' @usage
+#' \S4method{MarrAlpha}{Marr}(object)
+#'
+#' @docType methods
+#' @name MarrAlpha
+#' @rdname MarrAlpha
+#' @aliases MarrAlpha MarrAlpha,Marr-method
+#' @param object an object of class \code{Marr}.
+#'
+#' @return  Value of \code{alpha} argument passed to \code{Marr}
+#'
+#' @export
+#'
+#' @examples
+#' data <- matrix(rnorm(2400), nrow=200, ncol=12)
+#' data_Marr <- Marr(object = data, pSamplepairs=0.75,
+#'                   pFeatures=0.75, alpha=0.05)
+#' MarrAlpha(data_Marr)
+#'
+setMethod(f = "MarrAlpha", signature(object = "Marr"),
+          function(object) {
+              return(object@MarrAlpha)
+          })
+
+#' @title Accessors for the 'MarrFeatureVars' slot of a Marr object.
+#'
+#' @description Accessors for the 'MarrFeatureVars'
+#' slot of a Marr object.
+#'
+#' @usage
+#' \S4method{MarrFeatureVars}{Marr}(object)
+#'
+#' @docType methods
+#' @name MarrFeatureVars
+#' @rdname MarrFeatureVars
+#' @aliases MarrFeatureVars MarrFeatureVars,Marr-method
+#' @param object an object of class \code{Marr}.
+#'
+#' @return Value of \code{featureVars} passed to \code{Marr}. NULL
+#' if \code{featureVars} was left blank
+#'
+#' @export
+#'
+#' @examples
+#' data <- matrix(rnorm(2400), nrow=200, ncol=12)
+#' data_Marr <- Marr(object = data, pSamplepairs=0.75,
+#'                   pFeatures=0.75, alpha=0.05)
+#' MarrFeatureVars(data_Marr)
+#'
+setMethod(f = "MarrFeatureVars", signature(object = "Marr"),
+          function(object) {
+              return(object@MarrFeatureVars)
+          })
